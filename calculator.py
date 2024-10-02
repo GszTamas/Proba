9d4c7927ea33b390712eb7e0dbe083979612925d
Gubek-Szabo Tamas <gubek-szabo.tamas@diak.szbi-pg.hu>
2024-10-02 11:21:48 +0200

    add calculator file

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_substraction(a, b):
+    return a - b
+
+def calculate_multiplication(a, b):
+    return a * b
+
+def calculate_division(a, b):
+    return a / b
